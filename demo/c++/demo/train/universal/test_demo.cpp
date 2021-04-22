9d1e7cadbc8097d59e9f3c833866cabae8230705
wucj <wucj@fun.tv>
2021-04-22 19:54:42 +0800

    add variadic template and variadic function

--- demo/c++/demo/train/universal/test_demo.cpp
+++ demo/c++/demo/train/universal/test_demo.cpp
@@ -7,12 +7,19 @@
 void test_mutable_arg() {
     printf("%f\n", SumOfFloat(3, 1.2f, 3.4, 5.6));
 
-    printf("hello %s", std::string("world"));
+    printf("hello %s\n", std::string("world").c_str());
+    // 但是一下方式就不行
+    // error infomation: error: 'msg' was not declared in this scope
+    // constexpr char char *msg = "hello %s";
+    // printf(msg, std::string("world").c_str());
+
+    Printf("hello %s\n", std::string("world").c_str());
 }
 
 void test_demo()
 {
     test_mutable_arg();
+
 }
 
 double SumOfFloat(int count, ...)
@@ -25,4 +32,12 @@ double SumOfFloat(int count, ...)
 
     va_end(ap);
     return sum;
+}
+
+void Printf(const char *s) {
+    while(*s) {
+        if (*s == '%' && *++s != '%') 
+            throw std::runtime_error("invalid format string: missing arguments");
+        std::cout << *s++;
+    }
 }
--- demo/c++/demo/train/universal/test_demo.h
+++ demo/c++/demo/train/universal/test_demo.h
@@ -1,4 +1,21 @@
 _Pragma("once")
+#include <iostream>
+#include <stdexcept>
 
 double SumOfFloat(int count, ...);
 void test_demo();
+
+
+void Printf(const char *s);
+
+template<typename T, typename... Args>
+void Printf(const char* s, T value, Args... args) {
+    while(*s) {
+        if (*s == '%' && *++s != '%')  {
+            std::cout << value;
+            return Printf(++s, args...);
+        }
+        std::cout << *s++;
+    }
+    throw std::runtime_error("extra arguments provided to Printf");
+}
